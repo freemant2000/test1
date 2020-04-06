5df1971497abceecba3d399d5dc6562b32e2998a
Kent Tong <kent.tong.mo@gmail.com>
2020-04-06 17:58:35 +0800

    添加项目文件。

---
+++ test1/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Dim n As Integer = 0
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.M Then
+            PictureBox1.Height = PictureBox1.Height + 30
+
+
+
+        End If
+    End Sub
+
+
+End Class
